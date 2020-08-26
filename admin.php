5c859774aab300aa3bdc31f08d3119cb9201b1c1
sergeygvozdev08101993 <70273246+sergeygvozdev08101993@users.noreply.github.com>
2020-08-26 21:07:27 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,21 @@
+<?php
+    $username = 'sergeygvozdev';
+    $password = '1993';
+
+    if (!isset($_SERVER['PHP_AUTH_USER']) ||
+        !isset($_SERVER['PHP_AUTH_PW']) ||
+        ($_SERVER['PHP_AUTH_USER'] != $username) ||
+        ($_SERVER['PHP_AUTH_PW'] != $password)) {
+
+        header('HTTP/1.1 401 Unauthorized');
+        header('WWW-Authenticate: Basic-realm = Гитарные войны');
+        exit('<h2>Гитарные войны</h2>Извините, вы должны ввести' .
+        'правильные имя пользователя и пароль, чтобы получить доступ к этой странице.');
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang = 'ru'>
